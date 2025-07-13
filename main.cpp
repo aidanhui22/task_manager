230ce6ba18525a0a363428d136d57c38fc1c1851
aidan <139210594+aidanhui22@users.noreply.github.com>
2025-07-14 02:17:43 +0800

    added delete function, fixed edge cases, cleaned formatting

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Task {
 
 void display_menu();
 void add(std::vector<Task>&tasks, std::ofstream&outputFile);
-void del();
+void del(std::vector<Task>&tasks);
 void view(std::vector<Task>tasks);
 
 int main(void) {
@@ -34,7 +34,7 @@ int main(void) {
         if (x == 1) {
             add(tasks, outputFile);
         } else if (x == 2) {
-            del();
+            del(tasks);
         } else if (x == 3) {
             view(tasks);
         } else if (x == 4) {
@@ -49,6 +49,8 @@ int main(void) {
 
 void display_menu() {
     std::cout << '\n';
+    std::cout << "Enter the number corresponding to the following:" << std::endl;
+    std::cout << '\n';
     std::cout << "1. Add task\n";
     std::cout << "2. Delete task\n";
     std::cout << "3. View tasks\n";
@@ -59,20 +61,48 @@ void add(std::vector<Task>&tasks, std::ofstream&outputFile) {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::string userInput;
     std::cout << "Enter task description: ";
-    std::getline(std::cin, userInput);
-    outputFile << userInput << std::endl;
-    Task newTask{userInput, false};
-    tasks.push_back(newTask);
-    std::cout << "Task added!\n";
+    while (std::getline(std::cin, userInput)) {
+        outputFile << userInput << std::endl;
+        Task newTask{userInput, false};
+        tasks.push_back(newTask);
+        std::cout << "Task added!\n";
+        std::cout << '\n';
+        std::cout << "Enter task description: ";
+    }
 }
 
-void del() {
-    
+void del(std::vector<Task>&tasks) {
+    if (tasks.size() == 0) {
+        std::cout << "No tasks!" << std::endl;
+        return;
+    }
+    std::cout << "Which task to be deleted?" << std::endl;
+    view(tasks);
+    std::cout << std::endl;
+    int x = 0;
+    std::cin >> x;
+    std::vector<Task> temp;
+    for (int i = 0; i < tasks.size(); i++) {
+        Task newTask = tasks[i];
+        if (i != x) {
+            temp.push_back(newTask);
+        }
+    }
+    tasks = temp;
+    std::cout << "Task deleted!" << std::endl;
+    std::cout << '\n';
+    std::cout << "Updated list of tasks:" << std::endl;
+    std::cout << '\n';
+    view(tasks);
 }
 
 void view(std::vector<Task>tasks) {
+    if (tasks.size() == 0) {
+        std::cout << "No tasks!" << std::endl;
+        return;
+    }
     for (int i = 0; i < tasks.size(); i++) {
-        std::cout << "Task " << i + 1 << ": " << tasks[i].description << ", ";
+        std::cout << "Task " << i << ": " << tasks[i].description << ", ";
         if (tasks[i].completed == false) {
             std::cout << "Uncompleted.." << std::endl;
         } else {
